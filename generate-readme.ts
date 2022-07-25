a4fac433c9ea6cb9df307a3779e80527bb5c6d08
John Mars <jmars@deeplocal.com>
2022-07-25 16:17:01 -0400

    fix: issue with list concatenation

--- generate-readme.ts
+++ generate-readme.ts
@@ -54,56 +54,15 @@ export function generateReadme(list: List) {
 		md += `| Name | Locations | Keywords | up? |\n`;
 		md += `| ---- | --------- | -------- | --- |\n`;
 		md += sortedRows.map((row) => `| ${row[0]} | ${row[1]} | ${row[2]} | ${row[3]} |`).join("\n");
+		md += "\n\n";
 
 		return md;
 	});
 
 	// create the markdown
-	const md = mustache.render(template, { toc: contentsList, groups: groupsLists });
-
-	// 	const md = `
-	// <div align="center">
-	// 	<div>
-	// 		<a href="https://github.com/j0hnm4r5/awesome-creative-technology">
-	// 			<img width="500" src="./logo.gif" alt="Awesome Creative Technology Groups">
-	// 		</a>
-	// 	</div>
-	// 	<br>
-	// 	<p>
-	// 		Curated list of Creative Technology groups, companies, studios, collectives, etc.
-	// 	</p>
-	// 	<br>
-	// 	<a href="https://awesome.re">
-	// 		<img src="https://awesome.re/badge-flat2.svg" alt="Awesome">
-	// 	</a>
-	// 	<hr>
-	// </div>
-
-	// # Awesome Creative Technology
-
-	// > Businesses & groups that specialize in combining computing, design, art, and user experience.
-
-	// Creative technology is a broadly interdisciplinary and transdisciplinary field combining computing, design, art, and user experience.
-
-	// This list hopes to compile the best creative technology businesses & groups across the world, both as a source of inspiration and as a reference point for potential employers of creative technologists.
-
-	// Creative technologists by definition have a breadth of skills as opposed to a specific specialty, so it's difficult to categorize them. While this isn't a perfect organization, each group below generally specializes in the area to which they've been assigned.
-
-	// ## Table of Contents
-
-	// ${contentsList}
-
-	// ---
-
-	// ${groupsLists}
-
-	// ## Closed
-
-	// Groups that have closed their doors are still useful for reference and inspiration. Check out the list of them [here](closed.md).
-	// `;
+	const md = mustache.render(template, { toc: contentsList, groups: groupsLists.join("\n") });
 
 	fs.writeFileSync("readme.md", md);
-	// console.log(md);
 }
 
 export function generateClosedReadme(list: List) {
